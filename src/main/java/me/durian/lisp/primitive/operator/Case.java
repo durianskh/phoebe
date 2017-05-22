ad6be547a7296b90ea9fbf776ddcde3c20136213
durianskh <durianskh@gmail.com>
2017-05-22 22:45:00 +0800

    derived conditional and operators: 'and', 'case'

---
+++ src/main/java/me/durian/lisp/primitive/operator/Case.java
@@ -0,0 +1,9 @@
+package me.durian.lisp.primitive.operator;
+
+import me.durian.lisp.expression.derived.expression.DerivedConditional;
+
+/**
+ * Created by durianskh on 5/22/17.
+ */
+public interface Case extends Operator, DerivedConditional {
+}
--- src/main/java/me/durian/lisp/primitive/type/TypeProcedure.java
+++ src/main/java/me/durian/lisp/primitive/type/TypeProcedure.java
@@ -2,6 +2,8 @@ package me.durian.lisp.primitive.type;
 
 /**
  * Created by durianskh on 4/22/17.
+ * syntax:  (lambda <formals> <body>)
+ * Syntax: <Formals> should be a formal arguments list as described below, and <body> should be a sequence of one or more expressions
  */
 public interface TypeProcedure {
 }
